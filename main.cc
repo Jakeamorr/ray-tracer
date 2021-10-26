d47f9f8e85daa3af16490db0dfafde6a5c2e0d92
Jake Morris <Jakeamorr@gmail.com>
2021-10-26 13:08:03 -0400

    rename variables for main coordinates

--- main.cc
+++ main.cc
@@ -15,9 +15,9 @@ int main() {
     for (int y = image_height - 1; y >= 0; --y) {
         std::cerr << "\rScanlines remaining: " << y << ' ' << std::flush;
         for (int x = 0; x < image_width; ++x) {
-            color pixel_color(double(x) / (image_width - 1),
-                              double(y) / (image_height - 1),
-                              0.25);
+            double normalized_x_coord = double(x) / (image_width - 1);
+            double normalized_y_coord = double(y) / (image_height - 1);
+            color pixel_color(normalized_x_coord, normalized_y_coord, 0.25);
             write_color(std::cout, pixel_color);
         }
     }
