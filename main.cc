b8d969ef5d22fa45427f0b3f7347813c5e16d2aa
Jake Morris <Jakeamorr@gmail.com>
2021-10-28 10:25:16 -0400

    simpify sphere intersection codde by pulling out a 2 from all terms

--- main.cc
+++ main.cc
@@ -4,16 +4,17 @@
 #include "vec3.h"
 
 // Using the equation of sphere and vector algebra to determine whether or not a ray intersects our sphere.
+// We simplify to a quadratic equation -b +- sqrt(b^2 -4ac)/2a
 double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - 4 * a * c;
     if (discriminant < 0) {
         return -1.0;
     } else {
-        return (-b - sqrt(discriminant)) / (2 * a);
+        return (-half_b - sqrt(discriminant)) / a;
     }
 }
 
